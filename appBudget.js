8ee78420131fd2a2a59a3d785e1005434ac0b4df
Sylvie-B <unsolo@hotmail.fr>
2021-01-04 16:49:00 +0100

    get incomes

--- appBudget.js
+++ appBudget.js
@@ -1,2 +1,18 @@
 // get incomes
 let incomes = document.getElementById('moneyIncome').getElementsByTagName("input");
+let buttonIn = document.getElementById('addIncomes');
+let tIn = 0;
+
+buttonIn.addEventListener('click', function () {
+    for (let i = 0 ; i < incomes.length ; i++){
+        if(Number.isNaN(parseInt(incomes[i].value))){
+            alert('vous devez entrer des chiffres');
+        }
+        else {
+            tIn += parseInt(incomes[i].value);
+            incomes[i].value = '0';
+        }
+    }
+    document.getElementById('totalIncomes').innerHTML = tIn.toString()
+})
+
